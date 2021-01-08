aa20352cbb9682d36d792f6b7b63cde7992c4126
Kevin Aminzadeh <amin0015@flinders.edu.au>
2021-01-08 14:48:55 +1030

    refactor: Update criteria prompt implementation

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,57 +3,87 @@ var generateBtn = document.querySelector("#generate");
 
 // Generate Password
 function generatePassword() {
-  // Local Variables
-  let passwordCriteria = {
-    length: null,
-    lowercase: null,
-    uppercase: null,
-    numeric: null,
-    special: null,
+  // Local variables
+  const charset = {
+    0: {
+      name: "lowercase",
+      value: "abcdefghijklmnopqrstuvwxyz",
+    },
+    1: {
+      name: "uppercase",
+      value: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    },
+    2: {
+      name: "numeric",
+      value: "1234567890",
+    },
+    3: {
+      name: "special",
+      value: " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~",
+    },
   };
 
-  let charset = {
-    // ASCII Character Code Ranges For Each Type of Character
-    lowercase: [97, 122],
-    uppercase: [65, 90],
-    numeric: [48, 57],
-    special: {
-      0: [32, 47],
-      1: [58, 64],
-      2: [91, 96],
-      3: [123, 126],
-    },
+  let passwordCriteria = {
+    passLength: null,
+    allowedChars: "",
   };
 
   let pass = "";
 
-  // Get Password Critria from User
-  for (const criteria in passwordCriteria) {
-    if (criteria == "length") {
-      passwordCriteria[criteria] = prompt(
-        `Please enter the desired password ${criteria}: (Must be between 8 to 128 characters)`
-      );
+  // Get Password Length from User
+  passwordCriteria.passLength = prompt(
+    `Please enter the desired password length: (Must be between 8 to 128 characters)`
+  );
 
-      // Basic Input Validation - Check If password length is within the acceptable range, if not, return error.
-      if (passwordCriteria[criteria] < 8 || passwordCriteria[criteria] > 128) {
-        alert(
-          "Please enter a valid password length between 8 to 128 characters."
+  // IF the chosen password length is inside the allowed range then proceed to capturing user's desired character types for password
+  if (passwordCriteria.passLength >= 8 && passwordCriteria.passLength <= 128) {
+    for (const charType in charset) {
+      if (
+        confirm(
+          `Should the password include ${charset[charType].name} characters?`
+        )
+      ) {
+        passwordCriteria.allowedChars = passwordCriteria.allowedChars.concat(
+          charset[charType].value
         );
-        console.error(
-          "Invalid password length. Password must be between 8 to 128 characters."
-        );
-        return;
       }
-    } else {
-      passwordCriteria[criteria] = confirm(
-        `Should the password include ${criteria} characters?`
+    }
+  }
+  // Else reset the value of passLength, alert the user, throw an error and return
+  else {
+    passwordCriteria.passLength = null;
+    alert("Please enter a valid password length between 8 to 128 characters.");
+    console.error(
+      "Error: Invalid password length. Password must be between 8 to 128 characters."
+    );
+    return;
+  }
+
+  // Begin password generation only if at least one character type has been selected
+  if (passwordCriteria.allowedChars !== "") {
+    // Generate pseudorandom password based on user's selected criteria
+    for (let i = 0; i < passwordCriteria.passLength; i++) {
+      pass += passwordCriteria.allowedChars.charAt(
+        Math.floor(Math.random() * passwordCriteria.allowedChars.length)
       );
     }
   }
+  // Else throw an error, alert the user and return
+  else {
+    alert("Please select at least one character type.");
+    console.error(
+      "Error: No character type selected! You must select at least one character type."
+    );
+    return "Error: No character type selected! You must select at least one character type.";
+  }
+
+  return pass;
 }
 
 // Write password to the #password input
 function writePassword() {
+  // Clear Console errors from previous failed attempts
+  console.clear();
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
